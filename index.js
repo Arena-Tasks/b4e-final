ed9798b1006757acf37d97066c285e6be42c70b6
SalmanAd01 <salmanadhikari415@gmail.com>
2022-09-01 09:12:49 +0530

    Added scroll to top

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ for (var i = 0; i < btns.length; i++) {
     this.children[0].className = "nav-link active"
   })
 }
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 200);
+			return false;
+		});
+});
