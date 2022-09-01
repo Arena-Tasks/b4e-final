670517f80f04c9e2bc256efda61ba9dbd2f90301
SalmanAd01 <salmanadhikari415@gmail.com>
2022-09-01 09:36:27 +0530

    fix scroll

--- index.js
+++ index.js
@@ -6,19 +6,41 @@ for (var i = 0; i < btns.length; i++) {
     this.children[0].className = "nav-link active"
   })
 }
-$(document).ready(function(){
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('#back-to-top').fadeIn();
-			} else {
-				$('#back-to-top').fadeOut();
-			}
-		});
-		// scroll body to 0px on click
-		$('#back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 200);
-			return false;
-		});
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 200);
+    return false;
+  });
+});
+
+var TopOffset = 100;
+
+$('a[data-smooth]').click(function () {
+  var current = document.getElementsByClassName("active")
+  console.log(current[0],$(this).children('a'))
+  const selected = $(this).children('a')
+  console.log(selected.prevObject[0].className)
+  current[0].className = current[0].className.replace("nav-link active", "nav-link")
+  selected.prevObject[0].className = "nav-link active"
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top - TopOffset
+      }, 200);
+      return false;
+    }
+  }
+
 });
